4b394355b2a79d4d78dd2f786e6a3ffea15e68c7
Timur Guliamov <wersa2002@gmail.com>
2026-03-17 19:21:02 +0300

    Сделал работу

--- src/main.js
+++ src/main.js
@@ -5,7 +5,12 @@
  * @returns {number}
  */
 function calculateSimpleRevenue(purchase, _product) {
-   // @TODO: Расчет выручки от операции
+    const { discount, sale_price, quantity } = purchase;
+
+    const discountPercent = discount/100;
+    const totalPurchase = sale_price * quantity;
+
+    return totalPurchase * (1 - discountPercent);
 }
 
 /**
@@ -16,7 +21,16 @@ function calculateSimpleRevenue(purchase, _product) {
  * @returns {number}
  */
 function calculateBonusByProfit(index, total, seller) {
-    // @TODO: Расчет бонуса от позиции в рейтинге
+    const { profit } = seller;
+    if (index === 0) {
+        return profit * 0.15;
+    } else if (index === 1 || index === 2) {
+        return profit * 0.1;
+    } else if (index !== (total-1)) {
+        return  profit * 0.05;
+    } else {
+        return 0;
+    }
 }
 
 /**
@@ -26,19 +40,91 @@ function calculateBonusByProfit(index, total, seller) {
  * @returns {{revenue, top_products, bonus, name, sales_count, profit, seller_id}[]}
  */
 function analyzeSalesData(data, options) {
-    // @TODO: Проверка входных данных
 
-    // @TODO: Проверка наличия опций
+    // // @TODO: Проверка входных данных
+    if (!data
+        || (!Array.isArray(data.sellers) || !Array.isArray(data.products) || !Array.isArray(data.purchase_records))
+        || (data.sellers.length === 0 || data.products.length === 0 || data.purchase_records.length === 0)
+    ) {
+        throw new Error('Некорректные входные данные');
+    }
 
-    // @TODO: Подготовка промежуточных данных для сбора статистики
+    const { calculateRevenue, calculateBonus } = options;
 
-    // @TODO: Индексация продавцов и товаров для быстрого доступа
+    if (calculateRevenue === undefined || calculateBonus === undefined) {
+        throw new Error('Чего-то не хватает');
+    }
 
-    // @TODO: Расчет выручки и прибыли для каждого продавца
+    let mappedData = new Map();
+    data.purchase_records.forEach(record => {
+        if (!mappedData.has(record.seller_id)) {
+            mappedData.set(record.seller_id, {
+                seller_id: record.seller_id,
+                name: `${data.sellers.find(element => {
+                    return element.id === record.seller_id;
+                }).first_name} ${data.sellers.find(element => {
+                    return element.id === record.seller_id;
+                }).last_name}`,
+                revenue: 0, // ok
+                profit: 0, // ok
+                sales_count: 0, // ok
+                bonus: 0,
+                products_sold: new Map(),
+            });
+        }
+        mappedData.get(record.seller_id).sales_count+=1;
+        mappedData.get(record.seller_id).revenue+=record.total_amount;
 
-    // @TODO: Сортировка продавцов по прибыли
+        record.items.forEach(item => {
+            const product = data.products.find(element => {
+                return element.sku === item.sku;
+            }); // Товар
+            // Посчитать себестоимость (cost) товара как product.purchase_price, умноженную на количество товаров из чека
+            let cost = product.purchase_price * item.quantity;
+            // Посчитать выручку (revenue) с учётом скидки через функцию calculateRevenue
+            let revenue = calculateRevenue(item);
+            // Посчитать прибыль: выручка минус себестоимость
+            // Увеличить общую накопленную прибыль (profit) у продавца
+            mappedData.get(record.seller_id).profit+= (revenue - cost);
 
-    // @TODO: Назначение премий на основе ранжирования
+            if (!mappedData.get(record.seller_id).products_sold.has(item.sku)) {
+                mappedData.get(record.seller_id).products_sold.set(item.sku, 0);
+            }
+            mappedData.get(record.seller_id).products_sold.set(item.sku, mappedData.get(record.seller_id).products_sold.get(item.sku) + 1);
+        })
+    })
 
-    // @TODO: Подготовка итоговой коллекции с нужными полями
+    let sortedData = [...mappedData.entries()]
+        .sort((a, b) => {
+            if (a[1].profit > b[1].profit) {
+                return -1;
+            } else if (a[1].profit === b[1].profit) {
+                return 0;
+            } else {
+                return 1;
+            }
+        });
+
+    sortedData.forEach(([sellerId, seller], index) => {
+        seller.bonus = calculateBonus(index, sortedData.length, seller);
+        const productsBySku = new Map(
+            data.products.map(product => [product.sku, product])
+        );
+        seller.top_products = [...seller.products_sold.entries()]
+            .slice(0, 10)
+            .map(([sku]) => {
+                return productsBySku.get(sku);
+            });
+    });
+
+    return sortedData.map(([seller_id, entry]) => ({
+        seller_id,
+        name: entry.name,
+        revenue: +entry.revenue.toFixed(2),
+        profit: +entry.profit.toFixed(2),
+        sales_count: entry.sales_count,
+        top_products: entry.top_products,
+        bonus: +entry.bonus.toFixed(2),
+    }));
+
 }
